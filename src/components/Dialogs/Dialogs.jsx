70aa8122d1bdb953c1a881844784bca3be2699dc
R. Fariz <farizramazanov38@gmail.com>
2022-05-15 12:56:13 +0400

    feat: add content and some design to dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,23 @@
 import classes from "./Dialogs.module.css";
 
 const Dialogs = () => {
-    return (
-        <div>
-            Dialogs Page
-        </div>
-    )
-}
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>
+        <div className={`${classes.dialog} ${classes.active}`}>Eli</div>
+        <div className={classes.dialog}>Tofiq</div>
+        <div className={classes.dialog}>Vaqif</div>
+        <div className={classes.dialog}>Mamed</div>
+        <div className={classes.dialog}>Lale</div>
+        <div className={classes.dialog}>Nigar</div>
+      </div>
+      <div className={classes.messages}>
+          <div className={classes.message}>Hello</div>
+          <div className={classes.message}>Hi</div>
+          <div className={classes.message}>Bye</div>
+      </div>
+    </div>
+  );
+};
 
-export default Dialogs
+export default Dialogs;
