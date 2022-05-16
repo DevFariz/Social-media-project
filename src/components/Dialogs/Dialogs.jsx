66afa1ab6a655c9acd183f9db45f3c702f369c2e
R. Fariz <farizramazanov38@gmail.com>
2022-05-16 09:23:55 +0400

    fix: divide Dialogs into components

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,7 @@
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css";
-
-const DialogItem = (props) => {
-  return (
-    <div className={`${classes.dialog} ${classes.active}`}>
-      <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
-    </div>
-  );
-};
-
-const Message = (props) => {
-  return <div className={classes.message}>{props.message}</div>;
-};
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = () => {
 
